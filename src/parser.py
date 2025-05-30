a02861b05530a459eba7ec6db8686ec45a3d2cc9
desiredapple <savvinmark@mail.ru>
2025-05-30 16:58:11 +0300

    feat: write lamoda-parser version

--- src/parser.py
+++ src/parser.py
@@ -1,10 +1,60 @@
-from urllib.request import urlopen
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import requests
+import time
+import os
 
-url = "https://github.com/SimbiozizV/kino"
+options = uc.ChromeOptions()
+options.add_argument("--start-maximized")
 
-page = urlopen(url)
+driver = uc.Chrome(options=options)
 
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
+### LAMODA SECTION 
 
-print(html)
+driver.get("https://www.lamoda.ru/c/355/clothes-zhenskaya-odezhda/?sitelink=topmenuW&l=3")
+time.sleep(5)
+
+catalog = driver.find_elements(By.XPATH,'//div[contains(@class, "gridItem")]')
+
+
+print(catalog)
+
+for url in catalog:
+
+    driver.get(url)
+
+    try:
+        reviews_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//span[contains(@class, "ui-product-page-reviews-tab") and contains(text(), "Отзывы")]'))
+        )
+        reviews_button.click()
+
+        if not os.path.isdir("data"):
+            os.mkdir("data")
+
+        time.sleep(4)
+
+        images = driver.find_elements(By.XPATH,'//img[contains(@class, "ui-reviews-gallery")]')
+        
+        for i, img in enumerate(images):
+            cur_dir = f"data/{driver.title.split()[0]}"
+            src = img.get_attribute("src")
+            
+            if not os.path.isdir(cur_dir):
+                os.mkdir(cur_dir)
+            if src:
+                index = src.rfind(".")
+                filename = f"{cur_dir}/sample_{i}.{src[index:]}"
+                
+            img_data = requests.get(src).content
+            with open(filename, 'wb') as handler:
+                handler.write(img_data)
+
+    except Exception as e:
+        print("error:", e)
+
+### END LAMODA SECTION
+
+driver.quit()
