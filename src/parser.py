d18fbb395fc2c6c91853fafb69f48f631b923dbd
desiredapple <savvinmark@mail.ru>
2025-05-31 16:29:54 +0300

    style: correct tabs, line order

--- src/parser.py
+++ src/parser.py
@@ -8,41 +8,44 @@ import time
 import os
 
 options = uc.ChromeOptions()
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
-options.add_argument("--start-maximized")
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("--disable-background-networking")
-options.add_argument("--disable-sync")
 options.add_argument("--metrics-recording-only")
+options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-default-apps")
 options.add_argument("--disable-extensions")
+options.add_argument("--start-maximized")
+options.add_argument("--disable-sync")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_experimental_option(
     "prefs", {
-        "credentials_enable_service": False,
-        "profile.password_manager_enabled": False,
-        "profile.managed_default_content_settings.images": 2,
-        "profile.managed_default_content_settings.stylesheets": 2,
-        "profile.managed_default_content_settings.plugins": 2,
-        "profile.managed_default_content_settings.popups": 2,
-        "profile.managed_default_content_settings.geolocation": 2,
         "profile.managed_default_content_settings.notifications": 2,
-        "profile.managed_default_content_settings.media_stream": 2
+        "profile.managed_default_content_settings.media_stream": 2,
+        "profile.managed_default_content_settings.stylesheets": 2,
+        "profile.managed_default_content_settings.geolocation": 2,
+        "profile.managed_default_content_settings.plugins": 2,
+        "profile.managed_default_content_settings.images": 2,
+        "profile.managed_default_content_settings.popups": 2,
+        "profile.password_manager_enabled": False,
+        "credentials_enable_service": False
     }
 )
 
 browser = uc.Chrome(options=options, log_level=3)
 wait = WebDriverWait(browser, 7)
 
-### LAMODA SECTION
+# LAMODA SECTION
 for k in range(1, 14):
-    browser.get(f"https://www.lamoda.ru/c/355/clothes-zhenskaya-odezhda/?sitelink=topmenuW&l=3&page={k}")
+    browser.get(
+        f"https://www.lamoda.ru/c/355/clothes-zhenskaya-odezhda/?sitelink=topmenuW&l=3&page={k}")
 
-
-    wait.until(EC.presence_of_element_located((By.XPATH, '//a[contains(@class, "x-product-card__pic-catalog")]')))
-    page_catalog = [link.get_attribute('href') for link in browser.find_elements(By.XPATH,'//a[contains(@class, "x-product-card__pic-catalog")]')]
+    wait.until(EC.presence_of_element_located(
+        (By.XPATH, '//a[contains(@class, "x-product-card__pic-catalog")]')))
+    page_catalog = [link.get_attribute('href') for link in browser.find_elements(
+        By.XPATH, '//a[contains(@class, "x-product-card__pic-catalog")]')]
 
     for item in page_catalog:
 
@@ -50,7 +53,8 @@ for k in range(1, 14):
 
         try:
             reviews_button = wait.until(
-                EC.element_to_be_clickable((By.XPATH, '//span[contains(@class, "ui-product-page-reviews-tab") and contains(text(), "Отзывы")]'))
+                EC.element_to_be_clickable(
+                    (By.XPATH, '//span[contains(@class, "ui-product-page-reviews-tab") and contains(text(), "Отзывы")]'))
             )
             reviews_button.click()
 
@@ -59,43 +63,46 @@ for k in range(1, 14):
                 os.mkdir("data/lamoda")
 
             time.sleep(1)
-            
-            images = browser.find_elements(By.XPATH,'//img[contains(@class, "ui-reviews-gallery")]')
+
+            images = browser.find_elements(
+                By.XPATH, '//img[contains(@class, "ui-reviews-gallery")]')
             if not images:
-                images = browser.find_elements(By.XPATH,'//img[contains(@class, "photoAverage_9qw58_22")]')
+                images = browser.find_elements(
+                    By.XPATH, '//img[contains(@class, "photoAverage_9qw58_22")]')
 
             cur_dir = f"data/lamoda/{browser.title.split()[0]}"
             if not os.path.isdir(cur_dir):
                 os.mkdir(cur_dir)
-            
+
             i = 0
-            
+
             if len(images) > 5:
                 random.shuffle(images)
                 images = images[:5]
             elif not images:
                 continue
-            
+
             for img in images:
                 src = img.get_attribute('src')
                 if src.find('&') != -1:
                     src = src[:src.find('&')]
-                
+
                 if os.listdir(cur_dir):
-                    i = max([int(el[el.find('_') + 1 : el.find('.')]) for el in os.listdir(cur_dir)])
-                    
+                    i = max([int(el[el.find('_') + 1: el.find('.')])
+                            for el in os.listdir(cur_dir)])
+
                 if src:
                     index = src.rfind('.')
                     filename = f"{cur_dir}/sample_{i + 1}.{src[index + 1:]}"
-                
+
                 img_data = requests.get(src).content
-                    
+
                 with open(filename, 'wb') as handler:
                     handler.write(img_data)
 
         except Exception as e:
             print("error:", e)
 
-### END LAMODA SECTION
+# END LAMODA SECTION
 
 browser.quit()
